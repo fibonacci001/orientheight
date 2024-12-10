ab9a21179bd7132fa248256db77643c4b117d2f7
Gift Richard <giftrichard2000@gmail.com>
2024-12-10 15:45:22 +0100

    new change

--- src/libs/firebase/clientApp.js
+++ src/libs/firebase/clientApp.js
@@ -3,12 +3,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDbrYU_k76L0Z8cxIXhdO8R56TSUvxFWQw",
-  authDomain: "orientheight-crdb.firebaseapp.com",
-  projectId: "orientheight-crdb",
-  storageBucket: "orientheight-crdb.firebasestorage.app",
-  messagingSenderId: "365544503357",
-  appId: "1:365544503357:web:271d49cbba0ae2930dd254"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
