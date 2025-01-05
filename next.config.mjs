da4a58ad8c529422f94ede633a290a2615b4abeb
Gift Richard <giftrichard2000@gmail.com>
2025-01-05 07:59:23 +0100

    new

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',  // Add this line
+  output: 'standalone',  // Add this line
   reactStrictMode: false,
 
   images: {
